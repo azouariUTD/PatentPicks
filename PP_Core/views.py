d09a4895659a2dab39e0b88b1ed786c4fa34e0fa
Wolfgang <wolf_leclair7@tutanota.com>
2016-10-10 05:30:40 -0500

    Had to pull changes before pushing

--- PP_Core/views.py
+++ PP_Core/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Invention
 
 # Create your views here.
+def post_list(request):
+    inventions = Invention.objects.all()
+    #setlength = inventions.count()
+    #numCategories is for front end testing
+    return render(request, 'PP_Core/category.html', {'inventions':inventions}, {'numCategories' : range(7) })
+
+
--- PatentPicks/settings.py
+++ PatentPicks/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'PP_Core.apps.PpCoreConfig'
+    'PP_Core.apps.PpCoreConfig',
+
 ]
 
 MIDDLEWARE = [
