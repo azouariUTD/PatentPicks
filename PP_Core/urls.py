6bca3fa9b2bafd6f25e06cf41a3796d963c112ea
Wolfgang <wolf_leclair7@tutanota.com>
2016-10-11 06:27:59 -0500

    Made the discover page query the DB, made the links lead to pages, put in view functions and pages so each button goes to a filler page rather than a blank link, modified PP_Core.css to remove some unnecessasry functions

--- PP_Core/urls.py
+++ PP_Core/urls.py
@@ -2,5 +2,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [	
-    url(r'^$', views.post_list, name='post_list'),
+    url(r'^discover/', views.discover, name='discover'),
+    url(r'^howitworks/', views.howitworks, name ='howitworks'), 
+    url(r'^aboutus/', views.aboutus, name='aboutus'),
+    url(r'^login/', views.login, name='login'),
+    url(r'^register/', views.register, name='register'),
+    url(r'', views.home, name='home'),
 ]
--- PP_Core/views.py
+++ PP_Core/views.py
@@ -1,12 +1,33 @@
 from django.shortcuts import render
 from django.utils import timezone
-from .models import Invention
+from .models import Invention, Category
 
 # Create your views here.
-def post_list(request):
+def home(request):
     inventions = Invention.objects.all()
-    #setlength = inventions.count()
-    #numCategories is for front end testing
-    return render(request, 'PP_Core/category.html', {'inventions':inventions}, {'numCategories' : range(7) })
+    return render(request, 'PP_Core/home.html', {'inventions':inventions})
+
+def discover(request):
+    categories = Category.objects.all()
+    return render(request, 'PP_Core/discover.html', {"categories":categories})
+
+def howitworks(request):
+    title_from_view = "How it Works"
+    return render(request, 'PP_Core/howitworks.html', {"title_from_view":title_from_view})
+
+def aboutus(request):
+    title_from_view = "About us"
+    return render(request, 'PP_Core/aboutus.html', {"title_from_view":title_from_view})
+
+def login(request):
+    title_from_view = "Login"
+    return render(request, 'PP_Core/login.html', {"title_from_view":title_from_view})
+
+def register(request):
+    title_from_view = "Registration Page"
+    return render(request, 'PP_Core/register.html', {"title_from_view":title_from_view})
+
+
+
 
 
