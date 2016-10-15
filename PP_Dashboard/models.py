b2ef59d5dc1e03ac1b647a360f0717c514763678
Ahmed Zouari <asz130230@utdallas.edu>
2016-10-15 13:38:57 -0500

    Fixed Models

--- PP_Dashboard/models.py
+++ PP_Dashboard/models.py
@@ -1,14 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import User
-
 from PP_Core.models import InventionDetail
 
 
-class Account(models.Model):
+class Profile(models.Model):
     user = models.ForeignKey(User)
     royalty_income = models.DecimalField(max_digits=19, decimal_places=2, default=0.00)
     improvement_income = models.DecimalField(max_digits=19, decimal_places=2, default=0.00)
     funding_income = models.DecimalField(max_digits=19, decimal_places=2, default=0.00)
+    invention_funded = models.IntegerField()
+    inventions_saved = models.IntegerField()
+    inventions_hidden = models.IntegerField()
+    inventions_viewed = models.IntegerField()
+    inventions_not_viewed = models.IntegerField()
+    pledges_to_fund = models.IntegerField()
+    number_of_picks = models.IntegerField()
+    picks_in_production = models.IntegerField()
 
     def __str__(self):
         return self.user.first_name + " " + self.user.last_name + " " + self.user.email
