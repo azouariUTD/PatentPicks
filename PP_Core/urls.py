08ee97832350c67f3c31c52255b4971fd032daab
Shahrukh Zindani <szindani@nectarom.com>
2016-10-15 06:38:19 -0500

    Add logout url to it's corresponding view

--- PP_Core/urls.py
+++ PP_Core/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     url(r'^discover/', views.discover, name='discover'),
     url(r'^howitworks/', views.howitworks, name ='howitworks'), 
     url(r'^aboutus/', views.aboutus, name='aboutus'),
-    url(r'^login/', views.login, name='login'),
+    url(r'^login/', views.login_view, name='login'),
     url(r'^register/', views.register, name='register'),
     url(r'', views.home, name='home'),
 ]
