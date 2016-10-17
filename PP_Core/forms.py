e9465c8acd3c3c9a5dabcbae8f55fa144aa0a3b2
Shahrukh Zindani <szindani@nectarom.com>
2016-10-17 00:28:30 -0500

    Add User Login Form

--- PP_Core/forms.py
+++ PP_Core/forms.py
@@ -1,5 +1,11 @@
 from django import forms
 from .models import User
+from django.contrib.auth import (
+    authenticate,
+    get_user_model,
+    login,
+    logout
+)
 
 class RegisterForm(forms.Form):
     first_name = forms.CharField(label="first_name", required=True)
@@ -15,4 +21,23 @@ class RegisterForm(forms.Form):
         last_name = cleaned_data.get('last_name')
         email = cleaned_data.get('email')
         password = cleaned_data.get('password')
+
+User = get_user_model()
+
+class UserLoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget = forms.PasswordInput)
+
+    def clean(self, *args, **kwargs):
+        username = self.cleaned_data.get("username")
+        password = self.cleaned_data.get("password")
+        user = authenticate(username = username, password = password)
+
+        if not user:
+            raise forms.ValidationError("This user does not exist")
+        if not user.check_password(password):
+            raise forms.ValidationError("Incorrect Password")
+        if not user.is_active:
+            raise forms.ValidationError("This user no longer exists")
+        return super(UserLoginForm, self).clean( *args, **kwargs)
 
