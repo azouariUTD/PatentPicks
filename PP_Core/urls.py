7fc118c83bdf14f6e16f928f250b0fb075ab6714
Shahrukh Zindani <szindani@nectarom.com>
2016-10-17 00:25:56 -0500

    Add logout url

--- PP_Core/urls.py
+++ PP_Core/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     url(r'^howitworks/', views.howitworks, name ='howitworks'), 
     url(r'^aboutus/', views.aboutus, name='aboutus'),
     url(r'^login/', views.login_view, name='login'),
+    url(r'^logout/', views.logout_view, name='logout'),
     url(r'^register/', views.register, name='register'),
     url(r'', views.home, name='home'),
 ]
