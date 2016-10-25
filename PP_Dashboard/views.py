1c961e9f7c90e1d1eb21bbf264dbb7f142a83cea
Marc Tifrea <marc.tifrea@gmail.com>
2016-10-25 12:03:43 -0500

    Cleaned up files for join work

--- PP_Dashboard/views.py
+++ PP_Dashboard/views.py
@@ -3,6 +3,5 @@ from PP_Dashboard.models import Profile
 # Create your views here.
 def dashboard(request):
     title_from_view = "Dashboard"
-    person = Profile.objects.get(number_of_picks=19)
+    person = Profile() # Empty profile for now, getting current user will be done later
     return render(request, 'PP_Dashboard/dashboard.html', {"title_from_view": title_from_view, "person":person})
-
