b2cc19e57427d1bdc1acb8f5d72bb0e76bb5d976
Ahmed Zouari <asz130230@utdallas.edu>
2016-11-02 20:05:22 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	PP_Core/templates/PP_Core/base.html
    #	PP_Core/views.py
    #	PatentPicks/urls.py
    
    Everything is working as expected now!

--- PatentPicks/urls.py
+++ PatentPicks/urls.py
@@ -21,8 +21,10 @@ from django.conf.urls.static import static
 admin.site.site_header = settings.ADMIN_SITE_HEADER
 
 urlpatterns = [
+
     url(r'^admin/', admin.site.urls),
     url(r'^dashboard/', include('PP_Dashboard.urls')),
     url(r'^add_invention/', include('PP_Inventions.urls')),
     url(r'', include('PP_Core.urls')),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
