c21da5d046ca12c628b6d080e8a18a032603bf6a
Ahmed Zouari <asz130230@utdallas.edu>
2016-11-03 11:07:07 -0500

    Tweak so media files can be rendered

--- PP_Core/urls.py
+++ PP_Core/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     url(r'^login/', views.login_view, name='login'),
     url(r'^logout/', views.logout_view, name='logout'),
     url(r'^register/', views.register, name='register'),
-    url(r'', views.home, name='home'),
+    url(r'^$', views.home, name='home'),
 
 ]
 
