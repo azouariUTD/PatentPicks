91e0ea5ac5435d82f9347c3cca5519ee68f192d9
Marc Tifrea <marc.tifrea@gmail.com>
2016-11-12 11:30:52 -0600

    Start invention process

--- PP_Inventions/urls.py
+++ PP_Inventions/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 
 urlpatterns = [
+    url(r'^start_invention/get_started', views.get_started, name='get_started' ),
+    url(r'^start_invention/', views.start_invention, name='start_invention' ),
     url(r'', views.add_invention, name='add_invention'),
+
 ]
--- PP_Inventions/views.py
+++ PP_Inventions/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from PP_Core.models import Inventor
+from PP_Core.models import Inventor, Category
 from PP_Inventions.forms import InventionForm
 from django.views import generic
 
@@ -16,4 +16,22 @@ def add_invention(request):
         form = InventionForm()
 
     return render(request, 'PP_Inventions/add_invention.html', {'form': form})
+
+def start_invention(request):
+    categories = Category.objects.all()
+    categoryNames = []
+    categoryAmounts = []
+    for i in categories:
+        categoryNames.append(i.category_name)
+        categoryAmounts.append(i.quantity)
+    return render(request, 'PP_Inventions/start_invention.html', {"categoryAmounts":categoryAmounts,  "categoryNames":categoryNames})
+
+def get_started(request):
+    categories = Category.objects.all()
+    categoryNames = []
+    categoryAmounts = []
+    for i in categories:
+        categoryNames.append(i.category_name)
+        categoryAmounts.append(i.quantity)
+    return render(request, 'PP_Inventions/get_started.html',  {"categoryAmounts":categoryAmounts,  "categoryNames":categoryNames})
 
