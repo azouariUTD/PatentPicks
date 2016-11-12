94168de2d7b189d2c5f3d480793e975c90b4e22d
Marc Tifrea <marc.tifrea@gmail.com>
2016-11-12 11:37:05 -0600

    URL fixes

--- PP_Inventions/urls.py
+++ PP_Inventions/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     url(r'^start_invention/get_started', views.get_started, name='get_started' ),
     url(r'^start_invention/', views.start_invention, name='start_invention' ),
-    url(r'', views.add_invention, name='add_invention'),
+    url(r'^add_invention', views.add_invention, name='add_invention'),
 
 ]
